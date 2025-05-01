035312fd5da4eebbaa679ef003b28a542e388f19
Benciu Leonardo <benciuleonardo@gmail.com>
2025-05-01 15:14:34 +0300

    Fix routing with _redirects for client-side routing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
@@ -13,4 +12,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/',
+  server: {
+  }
 })
